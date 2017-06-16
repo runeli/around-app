612da0b8e32a35e5fb809d5cc08c920f396ceed2
Aleksanteri Negru-Vode <aleksanteri@negru-vode.fi>
2017-06-16 22:58:06 +0300

    handle adding arounds on the map

--- src/LeafletMap.js
+++ src/LeafletMap.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import LocationService from './LocationService';
 import PostActionButton from './PostActionButton';
-import HttpClient from './HttpClient';
 import {MapStyle} from './MapStyles';
-
+import ApplicationStateStore from './ApplicationStateStore';
 const VIEWPORT_INFORMATION_ID = 'viewPortId';
 const MAP_COMPONENT_ID = 'mapContainerId';
 
@@ -19,7 +18,7 @@ class LeafletMap extends React.Component {
     }
 
     componentWillUnmount() {
-        
+        ApplicationStateStore.removeStateChangeListerner(this._aroundStoreStateChangeListenerId);
     }
 
     _injectViewPortInformation() {
@@ -31,7 +30,7 @@ class LeafletMap extends React.Component {
     }
 
     async _injectMap() {
-        const google = window.google;
+        const google = window.google; 
         const coords = await LocationService.getCurrentLocation();          
         const center = {lat: coords.coords.latitude, lng: coords.coords.longitude};
         const map = new google.maps.Map(document.getElementById(MAP_COMPONENT_ID), {
@@ -44,23 +43,35 @@ class LeafletMap extends React.Component {
         map.addListener('click', () => {
             this._closeInfoBox()
         });
+        this._bindUpdateAroundsOnMapWhenNewAroundsAreAddedToApplicationStateStore(map);
+        this._addAroundsToMapAsMarkers(map, ApplicationStateStore.getClonedState().arounds);
+    }
 
-        const arounds = await HttpClient.getArounds();
+    _bindUpdateAroundsOnMapWhenNewAroundsAreAddedToApplicationStateStore(map) {
+        this._aroundStoreStateChangeListenerId = ApplicationStateStore.addStateChangeListener(aroundState => this._addAroundsToMapAsMarkers(map, aroundState.arounds));        
+    }
+
+    _addAroundsToMapAsMarkers(map, arounds) {
+        const google = window.google;
+        console.log(arounds);
         arounds.forEach(around => {
             const marker = new google.maps.Marker({
                 position: {lng: around.location.lng, lat:around.location.lat},
                 map: map,
                 title: around.messageBody
             });
-            marker.addListener('click', () => {
-                this._closeInfoBox();
-                this.infoWindow = new google.maps.InfoWindow({content: around.messageBody});
-                this.infoWindow.open(map, marker);
-            })
-        });                
+            this._bindInfoBoxClickingClosingPreviousInfoBoxAndOpeningNewOne(map, marker, around.messageBody);
+        });
     }
 
-
+    _bindInfoBoxClickingClosingPreviousInfoBoxAndOpeningNewOne(map, marker, messageBody) {
+        const google = window.google; 
+        marker.addListener('click', () => {
+            this._closeInfoBox();
+            this.infoWindow = new google.maps.InfoWindow({content: messageBody});
+            this.infoWindow.open(map, marker);
+        });
+    }
 
     _closeInfoBox() {
         if(this.infoWindow) {
